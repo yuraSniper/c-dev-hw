6bcc625db7e2f5b89741b2462812217c79cd0a95
Yuriy Stets <yuriy.y.stets@gmail.com>
2018-05-07 01:48:43 +0300

    Some progress on homework 3

--- hw3/src/alloc.c
+++ hw3/src/alloc.c
@@ -9,7 +9,6 @@
 
 static bool initialized = false;
 static container_descriptor * head[CONTAINER_KIND_COUNT];
-static container_descriptor * tail[CONTAINER_KIND_COUNT];
 static size_t alloc_size_threshold[CONTAINER_KIND_COUNT] =
 {
 	CONTAINER_SMALL_ITEM_SIZE,
@@ -27,9 +26,18 @@ static size_t alloc_size_threshold[CONTAINER_KIND_COUNT] =
 //NOTE(yura): Private functions:
 
 // chunk_alloc() - allocates a chunk of memory from specific container
-// This function requires single container of respective kind to be initialized
+// This function requires at least a single container of respective kind to be initialized
 static void * chunk_alloc(container_kind kind, size_t size)
 {
+	return NULL;
+}
+
+// container_alloc() - allocates a special container with a descriptor for it
+// This function requires at least a single generic container to allocate from
+static container_descriptor * container_alloc(container_kind kind)
+{
+	//NOTE(yura): alloc descriptor struct and alloc the container
+
 	return NULL;
 }
 
@@ -43,39 +51,65 @@ bool mstart()
 	// * allocate small containers
 	// * set initialized to true
 
-	void * generic_container = malloc(INITIAL_GENERIC_CONTAINER_SIZE);
+	void * heap = malloc(INITIAL_GENERIC_CONTAINER_SIZE);
 
-	if (generic_container == NULL)
+	if (heap == NULL)
 	{
 		fprintf(stderr, "newAlloc internal error: Cannot preallocate the required space (%u) for initial generic container\n", INITIAL_GENERIC_CONTAINER_SIZE);
 		return false;
 	}
 
-	chunk_header * chunk = (chunk_header *) generic_container;
+	chunk_header * chunk = (chunk_header *) heap;
 	chunk->length_flags = (sizeof(container_descriptor) & CHUNK_FLAGS_MASK) |
 	                      (CHUNK_FLAG_DESCRIPTOR);
 
 	container_descriptor * desc = (container_descriptor *) chunk->data;
-	desc->container_start = generic_container;
+	desc->container_start = heap;
 	desc->length_kind = (INITIAL_GENERIC_CONTAINER_SIZE	& CONTAINER_LENGTH_MASK) |
 	                    (CONTAINER_GENERIC);
-	desc->next = NULL;
-	desc->prev = NULL;
+	desc->next = desc;
+	desc->prev = desc;
 
 	head[CONTAINER_GENERIC] = desc;
 	chunk->owner_container = desc;
+
+	//NOTE(yura): Special container allocation:
+	
+	for (int index = CONTAINER_SMALL; index < CONTAINER_GENERIC; index++)
+	{
+		if (alloc_size_threshold[index] > 0)
+		{
+			container_descriptor * cont = container_alloc(index);
+			cont->prev = cont;
+			cont->next = cont;
+
+			head[index] = cont;
+		}
+	}
+
+	initialized = true;
 
 	return true;
 }
 
 void mstop()
 {
+	if (!initialized)
+	{
+		fprintf(stderr, "newAlloc internal error: Attempt to deinit a non initialized allocator\n");
+		return;
+	}
+	
 	initialized = false;
-	//dealloc small containers back-to-front
+
+	free(head[CONTAINER_GENERIC]->container_start);
 }
 
 void * alloc(uint32_t size)
 {
+	//NOTE(yura): Algorithm overview:
+	// * decide which container kind we should allocate from
+	// * find a container that can hold the chunk of requested size
 	return NULL;
 }
 
